5c4fbaa4ee2e5f860e01a3aef2791106113bca66
Nashir <nashiruddin.abubakar@gmail.com>
2021-03-02 18:29:57 +0700

    [CHG] added attribute and method for booking & driver model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Booking extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'driver_id',
+        'passenger_id',
+        'state',
+        'country_id',
+        'fare',
+        'created_at_local',
+    ];
+
+    public function getCreateAtLocalAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function driver()
+    {
+        return $this->hasOne(Driver::class, 'id', 'driver_id');
+    }
 }
--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'email',
+    ];
 }
