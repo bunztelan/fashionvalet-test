7827e85ad683b8ed0540ed9e9193b99a7d5e9b6a
Nashir <nashiruddin.abubakar@gmail.com>
2021-03-03 22:35:33 +0700

    [CHG] implement logic for question no 3

--- app/Http/Controllers/API/AddressController.php
+++ app/Http/Controllers/API/AddressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,75 @@ class AddressController extends Controller
     //
     public function submitAddress(Request $request)
     {
+        //retrieve input request
         $address_1 = $request->input("address_1");
         $address_2 = $request->input("address_2");
         $address_3 = $request->input('address_3');
+
+        //validate null input
+        if ($address_1 == null && $address_2 == null && $address_3 == null) {
+            return ResponseFormatter::error(
+                null,
+                "Please insert address",
+                403
+            );
+        }
+        // validate if all input is exist and each not more than 30
+        if ($this->validateAddress($address_1) && $this->validateAddress($address_2) && $this->validateAddress($address_3)) {
+            return ResponseFormatter::success(
+                null,
+                "Address is correct",
+            );
+        }
+
+        // validate one of the address is empty
+        // combine all addresses separated with space 
+        $address = $address_1 . " " . $address_2 . " " . $address_3;
+        //validate if combined address have more than 90 char
+        if (strlen($address) > 90) {
+            return ResponseFormatter::error(
+                $address,
+                "exceed address maximum length",
+                400,
+            );
+        }
+
+        return ResponseFormatter::success(
+            $this->formatAddress($address),
+            "Address formatted"
+        );
+    }
+
+    function validateAddress($address)
+    {
+        $length = strlen($address);
+        if ($length > 0 && $length < 30) {
+            return true;
+        }
+        return false;
+    }
+
+    function formatAddress($addresses)
+    {
+        $formattedAddress = ['', '', ''];
+        $arrayAddress = explode(" ", $addresses);
+        $index = 1;
+        $addressIndex = 0;
+        $next = $arrayAddress[$index];
+        foreach ($arrayAddress as $address) {
+            $formattedAddress[$addressIndex] .= $address;
+            // + 1 because we need to seperate the word with space
+            if ((strlen($formattedAddress[$addressIndex]) + strlen($next)) > 30) {
+                $addressIndex++;
+            } else {
+                $formattedAddress[$addressIndex] .= " ";
+            }
+            // check for the end of array
+            if ($index + 1 < count($arrayAddress)) {
+                $index++;
+                $next = $arrayAddress[$index];
+            }
+        }
+        return $formattedAddress;
     }
 }
